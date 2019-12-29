3bf0b049f4d301046e5ff1850c0db8266361a2cf
MaximGns <maxim.ganses@hotmail.com>
2019-12-29 09:27:49 +0100

    Added meta package for seo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
+@include('meta::manager')
+<!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
